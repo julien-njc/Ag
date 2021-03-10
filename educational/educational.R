afa05dc92849a394ec710ef363507b1f7b61cd5f
Hossein Noorazar <hn@Hosseins-iMac.local>
2021-03-09 17:04:38 -0800

    educational

--- educational/educational.R
+++ educational/educational.R
@@ -15,8 +15,8 @@ A[, c("date", "time") := tstrsplit(PriorityDate, " ", fixed=TRUE)]
 Extract row corresponding to minimum value of a variable by group
 
 data_dt <- data_dt %>%
-           group_by(lat, long, model, chill_season) %>% 
-           slice(which.min(chill_dayofyear)) %>%
+           group_by(location, chill_season, model, emission, time_period) %>% 
+           slice(which.min(thresh_55)) %>%
            data.table()
 
 data_dt[ , .SD[which.min(chill_dayofyear)], 
@@ -649,3 +649,109 @@ Above works also on data tables.
 
 # initiate data table of given size:
 data <- setNames(data.table(matrix(nrow = 0, ncol = 3)), c("va", "vb", "vc"))
+
+
+
+
+
+#######################################################################
+#
+#   How to add horizontal lines to facet plots and the legend!
+#
+
+ThreshCloud_2_rows_forForcing <- function(d1, trigger_dt){
+  
+  ##"""
+  ##
+  ## This function is modification of double_cloud_2_rows(.) to 
+  ## plot threshold with shadowy background for Springer. Tossing the damn bloom
+  ##
+  ##"""
+  
+  d1$chill_season <- gsub("chill_", "", d1$chill_season)
+  xlabels <- sort(unique(d1$chill_season))
+  xlabels <- xlabels[seq(1, length(xlabels), 15)]
+  xlabels <- c(xlabels) # , "2097-2098"
+
+  d1$chill_season <- substr(d1$chill_season, 1, 4)
+  d1$chill_season <- as.numeric(d1$chill_season)
+  xbreaks <- sort(unique(d1$chill_season))
+  xbreaks <- xbreaks[seq(1, length(xbreaks), 15)]
+  # xbreaks <- c(xbreaks, 2097)
+  # xbreaks <- c(xbreaks, 2100)
+  ylow = min(d1$value) - 5
+  ylow = 90
+
+  ymax = 220 # this is tuned by visual inspection!!
+  # ymax = min(ymax, max(d1$value))
+  
+  ggplot(d1, aes(x=chill_season, y=value, fill=factor(variable))) +
+  labs(x = "chill year", y = "day of year", fill = "data type") +
+  # guides(fill=guide_legend(title="")) + 
+
+  facet_wrap(. ~ location, scales="fixed") +
+  # geom_line(aes(fill=factor(Timeframe), color=factor(Timeframe) )) +
+  stat_summary(geom="ribbon", fun =function(z) { quantile(z,0.5) }, 
+                              fun.min=function(z) { quantile(z,0) }, 
+                              fun.max=function(z) { quantile(z,1) }, 
+               alpha=0.2) +
+
+  stat_summary(geom="ribbon", fun=function(z) { quantile(z,0.5) }, 
+                              fun.min=function(z) { quantile(z,0.1) }, 
+                              fun.max=function(z) { quantile(z,0.9) }, 
+               alpha=0.4) +
+
+  stat_summary(geom="ribbon", fun=function(z) { quantile(z,0.5) }, 
+                              fun.min=function(z) { quantile(z,0.25) }, 
+                              fun.max=function(z) { quantile(z,0.75) }, 
+               alpha=0.8) + 
+
+  stat_summary(geom = "line", fun=function(z) {quantile(z,0.5)}) +
+  # 
+  # plot horizontal lines for each city.
+  #
+  geom_hline(data = trigger_dt, 
+             aes(yintercept = median_heatTriggerThresh, 
+                 linetype = "heat accumulation trigger"), 
+             color = "red", 
+             size=1,
+             show.legend = TRUE
+             ) + 
+  scale_linetype_manual(name = "Limits", 
+                        labels = c("heat accumulation trigger"), 
+                        values = c("heat accumulation trigger" = 1)) +
+  
+  scale_color_manual(values=c("darkgreen"), breaks=c("thresh"), labels=c("CP threshold")) +
+  scale_fill_manual (values=c("darkgreen"), breaks=c("thresh"), labels=c("CP threshold")) +
+  
+  scale_x_continuous(breaks = xbreaks, label = xbreaks) +
+  scale_y_continuous(breaks = chill_doy_map$day_count_since_sept, 
+                     labels = chill_doy_map$letter_day) + 
+
+  theme(panel.grid.major = element_line(size=0.2),
+        panel.spacing = unit(.5, "cm"),
+        legend.text = element_text(size=18, face="bold"),
+        legend.title = element_blank(),
+        legend.position = "bottom",
+        strip.text = element_text(face="bold", size=16, color="black"),
+        axis.text = element_text(size=16, color="black"), # face="bold",
+        # axis.text.x = element_text(angle=20, hjust = 1),
+        axis.ticks = element_line(color = "black", size = .2),
+        axis.title.x = element_text(size=18,  face="bold", 
+                                    margin=margin(t=10, r=0, b=0, l=0)),
+        axis.title.y = element_text(size=18, face="bold",
+                                    margin=margin(t=0, r=10, b=0, l=0)),
+        plot.title = element_text(lineheight=.8, face="bold", size=20)
+        ) + # theme_bw() + 
+  coord_cartesian(ylim = c(ylow, ymax)) + 
+   guides(colour = guide_legend(override.aes = list(linetype = 0)),
+         fill = guide_legend(override.aes = list(linetype = 0)),
+         shape = guide_legend(override.aes = list(linetype = 0)),
+         linetype = guide_legend())
+
+}
+
+#######################################################################
+
+
+
